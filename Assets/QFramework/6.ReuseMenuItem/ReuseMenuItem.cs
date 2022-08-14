56a6352dd55ffdf944255f27fe4f195eddce2126
Semi-Tough <1693416984@qq.com>
2022-08-14 23:14:26 +0800

    Reuse MenuItem

---
+++ Assets/QFramework/6.ReuseMenuItem/ReuseMenuItem.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace QFramework
+{
+    public static class ReuseMenuItem
+    {
+#if UNITY_EDITOR
+        [MenuItem("QFramework/6.ReuseMenuItem")]
+        private static void Reuse()
+        {
+            EditorApplication.ExecuteMenuItem("QFramework/4.ExportUnityPackage");
+            EditorApplication.ExecuteMenuItem("QFramework/5.OpenInFolder");
+        }
+#endif
+    }
+}
