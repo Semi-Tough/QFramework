407edb2568e674e12594232008c9e3e3a91b0190
Semi-Tough <1693416984@qq.com>
2022-08-14 22:14:27 +0800

    Export UnityPackage

---
+++ Assets/QFramework/4.ExportUnityPackage/ExportUnityPackage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace QFramework
+{
+    public class ExportUnityPackage : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        [MenuItem("QFramework/4.ExportUnityPackage")]
+        private static void Export()
+        {
+            string assetPathName = "Assets/QFramework";
+            string fileName = "QFramework_" + DateTime.Now.ToString("yyyyMMdd_HH") + ".unitypackage";
+            AssetDatabase.ExportPackage(assetPathName, fileName, ExportPackageOptions.Recurse);
+        }
+#endif
+    }
+}
