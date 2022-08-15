74b6546fa06fd190ca9451d35b6f47cfed72e5c4
Semi-Tough <1693416984@qq.com>
2022-08-15 23:23:50 +0800

    Simply Local Position

---
+++ Assets/QFramework/10.SimplyLocalPosition/SimplyLocalPosition.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+
+
+namespace QFramework
+{
+    public class SimplyLocalPosition
+    {
+        public static void SetLocalPosX(Transform transform, float x)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.x = x;
+            transform.localPosition = localPos;
+        }
+
+        public static void SetLocalPosY(Transform transform, float y)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.y = y;
+            transform.localPosition = localPos;
+        }
+
+        public static void SetLocalPosZ(Transform transform, float z)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.z = z;
+            transform.localPosition = localPos;
+        }
+
+        public static void SetLocalPosXY(Transform transform, float x, float y)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.x = x;
+            localPos.y = y;
+            transform.localPosition = localPos;
+        }
+
+        public static void SetLocalPosXZ(Transform transform, float x, float z)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.x = x;
+            localPos.z = z;
+            transform.localPosition = localPos;
+        }
+
+        public static void SetLocalPosYZ(Transform transform, float y, float z)
+        {
+            Vector3 localPos = transform.localPosition;
+            localPos.y = y;
+            localPos.z = z;
+            transform.localPosition = localPos;
+        }
+    }
+}
