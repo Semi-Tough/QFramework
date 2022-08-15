f9c3cbf46c58adf6225c71b80eed3b015ecf2272
Semi-Tough <1693416984@qq.com>
2022-08-15 23:42:51 +0800

    Transform Identity

---
+++ Assets/QFramework/11.TransformIdentity/TransformIdentity.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace QFramework
+{
+    public class TransformIdentity
+    {
+        public static void Identity(Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localScale = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+        }
+    }
+}
