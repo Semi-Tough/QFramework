4afc656e9f76d8cabc6ec8d61e59c537faa827b9
Semi-Tough <1693416984@qq.com>
2022-08-15 23:54:04 +0800

    Simply GameObject SetActive

---
+++ Assets/QFramework/13.SimplyGameObjecSetActive/SimplyGameObjectSetActive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace QFramework
+{
+    public class SimplyGameObjectSetActive
+    {
+        public static void Show(GameObject gameObject)
+        {
+            gameObject.SetActive(true);
+        }
+
+        public static void Hide(GameObject gameObject)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
