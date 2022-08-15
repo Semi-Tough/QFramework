ea93dbcbf71ba3b11981b43706b2d12b798b9245
Semi-Tough <1693416984@qq.com>
2022-08-15 23:48:06 +0800

    Percent Function

---
+++ Assets/QFramework/12.PercnetFunction/PercentFunction.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace QFramework
+{
+    public class PercentFunction
+    {
+        public static bool Percent(int percent)
+        {
+            return Random.Range(0, 100) < percent;
+        }
+    }
+}
